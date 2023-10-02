a0042f3355f21beffe705c95d00cf1337c9d75c9
Ronak <guliani@pdx.edu>
2023-10-02 15:00:24 -0700

    finish palindrome

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,35 @@
 const elem = document.querySelector('input');
 
 elem.addEventListener('input', handleInput);
+
+const outputElement = document.querySelector('#output');
+
+function handleInput() {
+  const int = elem.value;
+  if (int < 0) {
+    outputElement.innerText = 'Please enter a positive number.';
+    outputElement.className = 'text-danger';
+    return;
+  }
+  const result = checkPalindrome(int);
+  if (result === false) {
+    outputElement.innerText = 'No. Try again.';
+    outputElement.className = 'text-danger';
+  }
+
+  else {
+    output.textContent = "Yes. This is a palindrome!";
+    outputElement.className = 'text-success';
+  }
+}
+
+function checkPalindrome(str) {
+  const len = str.length;
+  const mid = Math.floor(len / 2);
+  for (let i = 0; i < mid; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
