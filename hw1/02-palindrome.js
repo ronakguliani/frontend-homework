61851e056c8295cbd133ce35cd9ae57bb70ca09d
Ronak <guliani@pdx.edu>
2023-10-04 12:00:49 -0700

    Fix javascript typos and equality

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -5,20 +5,18 @@ elem.addEventListener('input', handleInput);
 const outputElement = document.getElementById('output');
 
 function handleInput() {
-  const int = elem.value;
+  const int = Number(elem.value);
   if (int < 0) {
     outputElement.innerText = 'Please enter a positive number.';
     outputElement.className = 'text-danger';
     return;
   }
-  const result = checkPalindrome(int);
-  if (result === false) {
-    outputElement.innerText = 'No. Try again.';
+  const result = checkPalindrome(int.toString());
+  if (!result) {
+    outputElement.textContent = 'No. Try again.';
     outputElement.className = 'text-danger';
-  }
-
-  else {
-    output.textContent = "Yes. This is a palindrome!";
+  } else {
+    outputElement.textContent = 'Yes. This is a palindrome!';
     outputElement.className = 'text-success';
   }
 }
