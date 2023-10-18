15c263aa533647376aa919f985fa96f6e0f13bf0
Ronak <guliani@pdx.edu>
2023-10-18 15:05:32 -0700

    Finish ex 5

--- hw2/01-flexbox.md
+++ hw2/01-flexbox.md
@@ -1,5 +1,3 @@
-## Exercise 01 - Flexbox Froggy
-
 1. justify-content: flex-end;
 2. justify-content: center;
 3. justify-content: space-around;
--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,26 @@
-// Add your code here
+const form = document.querySelector('form');
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const checkboxes = document.querySelectorAll('input');
+
+  if (form.elements.fullName.value) {
+    console.log('========== Form Submission ========== ');
+    console.log('Full Name:', form.elements.fullName.value);
+    console.log('Email:', form.elements.email.value);
+
+    console.log('Status: ', form.elements.registration.value);
+
+    console.log('Courses: ');
+    checkboxes.forEach((checkbox) => {
+      if (checkbox.checked) {
+        console.log('   ' + checkbox.value);
+      }
+    });
+
+    console.log('Anything Else:', form.elements.infoArea.value);
+  } else {
+    console.warn('Please enter your full name');
+  }
+};
